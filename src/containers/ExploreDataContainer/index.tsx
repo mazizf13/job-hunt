4c883a2bd3479ff0a63d6f5bfb2e64d1b9271681
mazizf13 <mukhamadazizfirmansyah@gmail.com>
2024-11-09 10:50:56 +0700

    feat: slicing find jobs (undone) 2

--- src/containers/ExploreDataContainer/index.tsx
+++ src/containers/ExploreDataContainer/index.tsx
@@ -9,32 +9,46 @@ interface ExploreDataContainerProps {
 
 const ExploreDataContainer: FC<ExploreDataContainerProps> = () => {
   return (
-    <div className="bg-gray-200 px-32 pt-16 pb-14">
-      <div className="mb-10">
-        <div className="mx-auto mb-11 text-center flex justify-center gap-2">
-          <span className="text-5xl font-semibold">Find Your</span>
-          <div className="relative">
-            <span className="text-5xl font-semibold text-primary">
-              Dream Job
-            </span>
-            <div className="absolute top-10 w-[220px] h-10">
-              <Image
-                src="/images/pattern2.png"
-                alt="/images/pattern2.png"
-                fill
-                objectFit="contain"
-              />
+    <>
+      <div className="bg-gray-200 px-32 pt-16 pb-14">
+        <div className="mb-10">
+          <div className="mx-auto mb-11 text-center flex justify-center gap-2">
+            <span className="text-5xl font-semibold">Find Your</span>
+            <div className="relative">
+              <span className="text-5xl font-semibold text-primary">
+                Dream Job
+              </span>
+              <div className="absolute top-10 w-[220px] h-10">
+                <Image
+                  src="/images/pattern2.png"
+                  alt="/images/pattern2.png"
+                  fill
+                  objectFit="contain"
+                />
+              </div>
             </div>
           </div>
+          <div className="text-center text-gray-500">
+            Find your next career at companies like Gojek, Traveloka, and
+            Halodoc.
+          </div>
         </div>
-        <div className="text-center text-gray-500">
-          Find your next career at companies like Gojek, Traveloka, and Halodoc
+        <div>
+          <div>Form Search</div>
         </div>
       </div>
-      <div>
-        <div>Form Search</div>
+
+      <div className="mt-20 mb-16 px-32 flex flex-row items-start gap-10">
+        <div>Form Filter Data</div>
+        <div className="w-4/5">
+          <div className="mb-8">
+            <div className="text-3xl font-semibold">All Jobs</div>
+            <div className="text-muted-foreground">Showing 73 Results</div>
+            <div>Job Card</div>
+          </div>
+        </div>
       </div>
-    </div>
+    </>
   );
 };
 
