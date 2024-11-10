5fad33a7ca614ba9464f05b07c6d697f1b50b82e
mazizf13 <mukhamadazizfirmansyah@gmail.com>
2024-11-10 21:52:56 +0700

    feat: setup form-schema modal apply job

--- src/lib/form-schema.ts
+++ src/lib/form-schema.ts
@@ -7,3 +7,22 @@ export const formFilterSchema = z.object({
 export const formFilterCompanySchema = z.object({
   industry: z.array(z.string()),
 });
+
+export const formApplySchema = z.object({
+  resume: z
+    .any()
+    .refine((file: any) => file?.name > 0, "Please upload your resume!"),
+  fullname: z
+    .string({ required_error: "Please enter your name!" })
+    .min(5, "Fullname must be at least 5 characters!"),
+  email: z
+    .string({ required_error: "Please enter your email!" })
+    .email("Please enter a valid email!"),
+  phone: z
+    .string({ required_error: "Please enter your phone number!" })
+    .min(8, "Phone number must be at least 8 characters!"),
+  previousJobTitle: z.string(),
+  linkedIn: z.string(),
+  portfolio: z.string(),
+  coverLetter: z.string(),
+});
