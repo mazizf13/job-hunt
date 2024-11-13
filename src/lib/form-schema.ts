14b10f5bb4c337ef9a02596a0a7fbc3b60af232a
mazizf13 <mukhamadazizfirmansyah@gmail.com>
2024-11-13 10:16:24 +0700

    feat: slicing signup

--- src/lib/form-schema.ts
+++ src/lib/form-schema.ts
@@ -39,3 +39,15 @@ export const formSignInSchema = z.object({
     .string({ required_error: "Please enter your password!" })
     .min(8, "Password must be at least 8 characters!"),
 });
+
+export const formSignUpSchema = z.object({
+  name: z
+    .string({ required_error: "Please enter your name!" })
+    .min(3, "Name must be at least 3 characters!"),
+  email: z
+    .string({ required_error: "Please enter your email!" })
+    .email("Please enter a valid email!"),
+  password: z
+    .string({ required_error: "Please enter your password!" })
+    .min(8, "Password must be at least 8 characters!"),
+});
